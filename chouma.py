346cc5117736830a26f909a999e018a0a6fe9951
sun <19996240@qq.com>
2020-08-07 00:05:37 +0800

    85

--- chouma.py
+++ chouma.py
@@ -3,6 +3,9 @@ import re, string
 import os
 import glob
 import math
+import random
+
+
 import pandas as pd
 import time# 1. 获取时间元组
 struct_time = time.localtime()# 2. 转换字符串格式
@@ -12,6 +15,79 @@ cm_dics    = {}     #    0: {'2000-01-01',100}
 today_dics = {}
 
 base_diretory="data\\"
+
+
+
+
+def get_weight_hl (  hl):
+        weight_hl = 0 #获利的权重
+        weight_hsl = 0  # 换手率的权重
+
+        weight= 0
+        if (hl<0 and hl>=-5):
+            weight_hl=(0.5,0.8)
+        elif(hl< -5 and hl>=-10 ):
+            weight_hl = (0.3, 0.5)
+        elif(hl< -10 and hl>=-20 ):
+            weight_hl = (0.1, 0.2)
+        elif(hl<-20 and hl>=-30 ):
+            weight_hl = (0.05, 0.08)
+        elif(hl< -30 ):
+            weight_hl = (0.01, 0.02)
+        elif(hl< 5 and hl>=0 ):
+            weight_hl = (0.85, 0.9)
+        elif (hl < 10 and hl >= 5):
+            weight_hl = (0.65, 0.8)
+        elif (hl < 15 and hl >= 10):
+            weight_hl = (0.3, 0.5)
+        elif (hl < 20 and hl >= 15):
+            weight_hl = (0.1, 0.2)
+        elif (hl < 30 and hl >= 20):
+            weight_hl = (0.05, 0.08)
+        elif ( hl >=30):
+            weight_hl = (0.01, 0.03)
+        return weight_hl
+
+def get_weight_day(day):
+        weight_day = 0
+        if (day < 3 and day >= 0):
+            weight_day = (0.6, 0.8)
+        elif(day< 5 and day>=3 ):
+            weight_day = (0.5, 0.6)
+        elif (day < 10 and day >= 5):
+            weight_day = (0.4, 0.7)
+        elif (day < 15 and day >= 10):
+            weight_day = (0.3, 0.5)
+        elif (day < 20 and day >= 15):
+            weight_day = (0.1, 0.2)
+        elif (day < 30 and day >= 20):
+            weight_day = (0.06, 0.08)
+        elif (day < 50 and day >= 30):
+            weight_day = (0.03, 0.05)
+        elif ( day >= 50):
+            weight_day = (0.01, 0.02)
+        return weight_day
+
+def get_weight_hsl(hsl):
+        weight_hsl= 0
+        if (hsl < 3 and hsl >= 0):
+            weight_hsl = (0.03, 0.05)
+        elif(hsl< 5 and hsl>=3 ):
+            weight_hsl = (0.06, 0.08)
+        elif (hsl < 10 and hsl >= 5):
+            weight_hsl = (0.08, 0.12)
+        elif (hsl < 15 and hsl >= 10):
+            weight_hsl = (0.15, 0.3)
+        elif (hsl < 20 and hsl >= 15):
+            weight_hsl = (0.3, 0.5)
+        elif (hsl < 30 and hsl >= 20):
+            weight_hsl = (0.5, 0.6)
+        elif (hsl < 50 and hsl >= 30):
+            weight_hsl = (0.7, 0.8)
+        elif ( hsl >= 50):
+            weight_hsl = (0.9, 0.95)
+        return weight_hsl
+
 
 
 
@@ -78,8 +154,9 @@ def deal_gp_fb(data_file, rq, stk_code):
 
 
 
-
-
+#转换存储的目录
+def get_dir(rq):
+   return rq[:7].replace("-","")
 
 
 def main():
@@ -132,8 +209,44 @@ def getRq(dm):
 
 if __name__ == '__main__':
     #main()
+    aa= get_weight_hsl(3.5)
+
+    print(aa[0], aa[1])
+    a=random.randint(aa[0] * 100, aa[1] * 100) / 100
+    print(a)
+
+    bb=get_weight_day(13)
+    print(bb[0], bb[1])
+    b=random.randint(bb[0] * 100, bb[1] * 100) / 100
+    print()
+
+
+
+
+    cc=get_weight_hl(17)
+    print(cc[0], cc[1])
+    c=random.randint(cc[0] * 100, cc[1] * 100) / 100
+    print(c)
+
+    print(a*b*c)
+
+
+
+
+
+
+
+
+
+
+    ''' 
     dm="300842"
     rq=getRq(dm)
     print(rq)
     ltp = get_ltp(dm)
     print( ltp )
+   
+  
+    print(get_dir(rq[0]))
+    
+    '''
